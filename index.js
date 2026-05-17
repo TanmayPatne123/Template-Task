0d9a87a545a0b48b5e9079f2ca9510bcfa2d0ed4
Tanmay Patne <142735230+TanmayPatne123@users.noreply.github.com>
2026-05-17 16:15:58 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,31 @@
+// let a = setTimeout(function(){
+//     alert("I am inside timeout")
+// },5000);
+
+// let b = prompt("do want set Timeout");
+// if("n"==b)
+//     {
+//     clearTimeout(a);
+// }
+
+// function printTime(){
+//     let time = new Date().toLocaleTimeString();
+//     console.log(time);
+// }
+
+// setInterval(printTime,2000);
+
+
+// let num=0;
+// function printNumber(){
+//     num++;
+//     if(num>=3)
+//         {
+//             clearInterval(store);
+//         }
+//         console.log(num);
+// }
+
+// let store= setInterval(printNumber,1000);
+
+
